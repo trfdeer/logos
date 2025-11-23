a236ee39b9a64ed1dbc8b0478d3f4e37e74e6930
ttarafder <ttarafder7d1@protonmail.com>
2025-11-23 16:03:47 +0530

    add wsl config

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
       url = "github:nix-community/home-manager?ref=release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
   };
 
   outputs =
@@ -22,7 +23,7 @@
         username = "ttarafder";
         email = "ttarafder7d1@protonmail.com";
         signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILNyNZFAlLgqaLZGIB79Gz/FT0rj9PcIYW6zaM4fhUhb";
-        sshKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILNyNZFAlLgqaLZGIB79Gz/FT0rj9PcIYW6zaM4fhUhb" ];
+        sshKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBod7BQCA2N5GCdkD8NJzjhx5uajVrUwNCok+EYtDAvA" ];
         stateVersion = "25.05";
       };
       pkgs = import nixpkgs {
@@ -36,7 +37,18 @@
         specialArgs = { inherit inputs constants; };
 
         modules = [
-          ./hosts/nixvm1/configuration.nix
+          inputs.nixos-wsl.nixosModules.default
+          ./hosts/wsl/configuration.nix
+          ./modules/nixos
+        ];
+      };
+
+      nixosConfigurations.wsl = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs constants; };
+
+        modules = [
+          ./hosts/wsl/configuration.nix
           ./modules/nixos
         ];
       };
