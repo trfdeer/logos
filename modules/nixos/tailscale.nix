d4aee08786f0805d56199d8e4d769b32c83a5d20
Tuhin Tarafder <ttarafder7d1@protonmail.com>
2025-12-27 17:13:15 +0530

    update tailscale config and remove incus

--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -7,19 +7,37 @@
         type = lib.types.nonEmptyStr;
         description = "Set tailscale operator";
       };
+      advertiseRoutes = lib.mkOption {
+        type = lib.types.str;
+        default = "";
+        description = "Advertise subnet routes";
+      };
     };
   };
 
   config = lib.mkIf config.sqwer.tailscale.enable {
     services.tailscale = {
       enable = true;
+      useRoutingFeatures = "both";
       extraSetFlags = [
         "--ssh=false"
-        "--accept-dns"
-        "--accept-routes"
+        "--accept-dns=true"
+        "--accept-routes=true"
         "--netfilter-mode=nodivert"
         "--operator=${config.sqwer.tailscale.operator}"
+      ]
+      ++ lib.optionals (config.sqwer.tailscale.advertiseRoutes != "") [
+        "--advertise-routes=${config.sqwer.tailscale.advertiseRoutes}"
+        "--snat-subnet-routes=false"
       ];
+      extraDaemonFlags = [ "--no-logs-no-support" ];
+    };
+
+    boot.kernel.sysctl = lib.mkIf (config.sqwer.tailscale.advertiseRoutes != "") {
+      "net.ipv4.conf.all.forwarding" = true;
+      "net.ipv4.conf.default.forwarding" = true;
+      "net.ipv6.conf.all.forwarding" = true;
+      "net.ipv6.conf.default.forwarding" = true;
     };
   };
 }
