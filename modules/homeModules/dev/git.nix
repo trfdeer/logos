50d4705b97bfd8f5b1c4975e6e874cd3c348e2b7
Tuhin Tarafder <62106295+trfdeer@users.noreply.github.com>
2026-01-20 18:37:06 +0530

    fix(ci): use plain string for op-ssh-sign-wsl path (#17)

--- modules/homeModules/dev/git.nix
+++ modules/homeModules/dev/git.nix
@@ -47,7 +47,7 @@ in
       // lib.optionalAttrs (homeCfg.git.user.signingkey != "" && config.sqwer.env.has1Password) {
         "gpg \"ssh\"".program =
           if config.sqwer.env.isWsl then
-            ''/mnt/c/Program Files/1Password/app/8/op-ssh-sign-wsl''
+            "/mnt/c/Program Files/1Password/app/8/op-ssh-sign-wsl"
           else
             "/opt/1Password/op-ssh-sign";
       }
