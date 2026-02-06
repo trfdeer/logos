90512988e5a5f5fc23870c232e6336b50dbfa1ca
Tuhin Tarafder <62106295+trfdeer@users.noreply.github.com>
2026-02-06 07:47:40 +0530

    chore: update flake inputs and terraria server (#21)
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'catppuccin':
        'github:catppuccin/nix/d7a8632c0d8d144478aac1a8c8d5083b770cbb03?narHash=sha256-q4arZjXnLiuMnLzO972lrXIGdzyGb4DGaIt69CcCYdE%3D' (2026-01-26)
      → 'github:catppuccin/nix/ff59ad5085b7d71705ea1f019c02b08dfe40d9c8?narHash=sha256-cjPsPx3qlP7RW78lcGJoTP/B6yqY6BJJjwT7QUg8ByY%3D' (2026-02-01)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/366d78c2856de6ab3411c15c1cb4fb4c2bf5c826?narHash=sha256-tNqCP/%2B2%2BpeAXXQ2V8RwsBkenlfWMERb%2BUy6xmevyhM%3D' (2026-01-28)
      → 'github:nix-community/home-manager/0d782ee42c86b196acff08acfbf41bb7d13eed5b?narHash=sha256-3iVX1%2B7YUIt23hBx1WZsUllhbmP2EnXrV8tCRbLxHc8%3D' (2026-02-05)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/fa83fd837f3098e3e678e6cf017b2b36102c7211?narHash=sha256-e7VO/kGLgRMbWtpBqdWl0uFg8Y2XWFMdz0uUJvlML8o%3D' (2026-01-28)
      → 'github:nixos/nixpkgs/3b91d3991171d330eceba85fae28cad7d22f95ac?narHash=sha256-kejbY45kK53xKRzqKbiy%2BAxU/6FMLuzQjW9uJTEDF7U%3D' (2026-02-05)
    
    * chore(flake): pin catppuccin to release-25.11
    
    * chore(terraria): bump server to 1.4.5.4

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     catppuccin = {
-      url = "github:catppuccin/nix";
+      url = "github:catppuccin/nix/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lanzaboote = {
