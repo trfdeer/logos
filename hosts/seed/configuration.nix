2cbb6482d9c2a328b982cb45a55273930ee7f383
Tuhin Tarafder <ttarafder7d1@protonmail.com>
2026-05-30 09:25:07 +0530

    set installer iso boot timeout

--- hosts/seed/configuration.nix
+++ hosts/seed/configuration.nix
@@ -15,6 +15,7 @@ in
   installer.cloneConfig = false;
 
   boot.kernelParams = [ "console=ttyS0,115200" ];
+  boot.loader.timeout = 10;
   networking.hostName = "seed";
 
   sqwer.system.ssh.enable = true;
